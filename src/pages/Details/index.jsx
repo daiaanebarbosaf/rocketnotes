ca9f8515834bbf4608af4f32878ae01dfa99fa8f
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-03 08:21:48 -0300

    applying styled components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,9 +2,9 @@ import { Container } from './style';
 
 export function Details(){
   return(
-    <div>
+    <Container>
       <h1>Hello Word!</h1>
       <span>Rodrigo Gonçalves</span>
-    </div>
+    </Container>
   )
 }
