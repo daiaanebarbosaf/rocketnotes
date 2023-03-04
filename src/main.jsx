a11d45ebd8c68c800fdbb131a4809f9293949091
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-04 08:20:21 -0300

    importing global styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider} from 'styled-components';
+import theme from './styles/global'
 import theme from './styles/theme'
 
 import { Details } from './pages/Details';
