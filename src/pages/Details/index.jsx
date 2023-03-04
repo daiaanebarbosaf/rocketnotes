5e79836051f53eb227944c80adb8baedf4553bfe
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-04 11:41:56 -0300

    importing the button to the main page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
 import { Container } from './style';
+import { Button } from '../../components/Button';
 
 export function Details(){
   return(
     <Container>
       <h1>Hello Word!</h1>
       <span>Rodrigo Gonçalves</span>
+      <Button />
     </Container>
   )
 }
