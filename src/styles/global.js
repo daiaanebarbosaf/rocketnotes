e46322c5ef12ef0410bcae9767bd3f0eeebc8aaf
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-04 08:40:36 -0300

    first styling tweaks

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,7 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  *{
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
@@ -9,5 +9,19 @@ export default createGlobalStyle`
 
   body {
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9);
   }
 `;
