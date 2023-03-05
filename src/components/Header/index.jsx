273e559bbf86bcecf1bfe0d5520bdbb393669230
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-05 07:23:37 -0300

    add header contents

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,19 @@
-import { Container } from './style';
+import { Container, Profile } from './style';
 
 export function Header(){
   return(
     <Container>
+      <Profile>
+        <img 
+          src="https://github.com/rodrigorgtic.png" 
+          alt="Foto do usuário" 
+        />
 
+        <div>
+          <span>Bem-vindos,</span>
+          <strong>Rodrigo Gonçalves</strong>
+        </div>
+      </Profile>
     </Container>
   );
 }
