f5a193c732cca040c082003b50a8dee99acab34d
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-06 08:59:04 -0300

    import section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,19 @@
-import { Container } from './style';
+import { Container, Links } from './style';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
 
 export function Details(){
   return(
     <Container>
       <Header />
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">Link 1</a>
+          </li>
+        </Links>
+      </Section>
       <Button title="Voltar"/>
     </Container>
   )
