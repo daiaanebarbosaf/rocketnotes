f41a07f690767de1ff04ebd24ff1deab9b9bf881
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-08 06:36:08 -0300

    Update index.jsx

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './style';
+import { Container, Links, Content } from './style';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
@@ -10,25 +10,28 @@ export function Details(){
   return(
     <Container>
       <Header />
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <ButtonText title="Excluir nota" />
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
 
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href="#">https://www.rocketseat.com.br/</a>
-          </li>
-          <li>
-            <a href="#">https://www.rocketseat.com.br/</a>
-          </li>
-        </Links>
-      </Section>
-
-      <Section title="Marcadores">
-        <Tag title="express"/>
-        <Tag title="nodejs"/>
-      </Section>
-      <Button title="Voltar"/>
+          <Section title="Marcadores">
+            <Tag title="express"/>
+            <Tag title="nodejs"/>
+          </Section>
+          <Button title="Voltar"/>        
+        </Content>
+      </main>
     </Container>
   )
 }
