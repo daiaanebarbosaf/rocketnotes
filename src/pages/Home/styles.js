4f48f1f2319a080a00f5bd2e2aadcb6d155fee74
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-08 09:05:04 -0300

    Create styles.js

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-columns: 250px auto;
+`;
+
+export const Brand = styled.div``;
+
+export const Menu = styled.ul``;
+
+export const Search = styled.div``;
+
+export const Content = styled.div``;
+
+export const NewNote= styled.button``;
