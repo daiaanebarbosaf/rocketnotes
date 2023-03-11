ebedb4053e78c79ad442c5c76bd326750e0cd8b6
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-03-11 11:31:03 -0300

    importing note in the home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,8 +2,10 @@ import { FiPlus, FiSearch } from 'react-icons/fi';
 
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
+import { Note } from '../../components/Note';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
+import { Section } from '../../components/Section';
 import { ButtonText } from '../../components/ButtonText';
 
 export function Home(){
@@ -28,7 +30,16 @@ export function Home(){
       </Search>
 
       <Content>
-
+        <Section title="Minhas notas">
+          <Note data={{
+            title: 'React',
+            tags: [
+              {id: '1', name: 'react'},
+              {id: '2', name: 'rocketseat'}
+            ]
+          }}
+          />     
+        </Section>
       </Content>
 
       <NewNote>
